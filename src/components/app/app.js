64d48674f7e853c5fdf71568c019d647620f03d9
v.shorinadm <voldemar-96@mail.ru>
2022-08-04 19:00:50 +1000

    Создана структура проекта

---
+++ src/components/app/app.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Navbar from '../navbar';
+
+const App = () => {
+  return (
+      <Navbar />
+    /*<div>
+        <Navbar />
+        <h1>test</h1>
+    </div>*/
+  );
+}
+
+export default App;
---
+++ src/components/navbar/navbar.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Navbar = () => {
+    return (
+        <h1>test</h1>
+    );
+}
+
+export default Navbar;
